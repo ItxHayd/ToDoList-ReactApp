b4c094414ebaa85417771803cf739712e54e1b82
hayd <haydshafqat2@gmail.com>
2026-02-15 10:27:19 +0500

    deployment

--- src/AddTask.tsx
+++ src/AddTask.tsx
@@ -30,7 +30,7 @@ function AddTask({inputText,BtnContent}:Props){
         <div className="w-2/3 flex justify-center content-center mx-auto mt-3 realtive">
             <div className="flex flex-1 relative">
                 <input onChange={(e)=>{setTaskValue(e.target.value)}} type="text" className="bg-gray-600 p-2 w-full absolute rounded-md font-semibold mr-1" placeholder={inputText}/>
-                <img src="src\assets\cross.svg" onClick={Delete} className="cursor-pointer h-11 flex z-2 absolute right-0 active:scale-110" id="deleteBtn"/>
+                <img src="\cross.svg" onClick={Delete} className="cursor-pointer h-11 flex z-2 absolute right-0 active:scale-110" id="deleteBtn"/>
             </div>
             <button id="AddTaskBtn" onClick={HandleAddTask} className="cursor-pointer bg-red-500 font-semibold  rounded-md active:scale-105 active:bg-emerald-500 p-2 transition duration-100">{BtnContent}</button>
 
--- src/tasks.tsx
+++ src/tasks.tsx
@@ -12,7 +12,7 @@ function Tasks({value}:Props){
             <tr className="flex flex-1 w-9/12 mx-auto mb-2 ">
                 <td className="w-full">
                     <div className="bg-gray-600 p-2 w-full relative rounded-md font-semibold mr-1">{value}
-                        <img src="src\assets\cross.svg" className=" h-10 z-2 absolute top-0 right-0 active:scale-110 cursor-pointer in" id="deleteBtn" onClick={handleDelete}/>
+                        <img src="\cross.svg" className=" h-10 z-2 absolute top-0 right-0 active:scale-110 cursor-pointer in" id="deleteBtn" onClick={handleDelete}/>
                     </div>
                 </td>  
             </tr>
